041abb63622106da22b1ff6c6c1d03e1941c9e3d
Gaurav Munjal <Gaurav0@aol.com>
2013-02-20 20:46:34 -0500

    Added ability to combine certain torso elements; added formal menswear; separated armor
    Signed-off-by: Gaurav Munjal <Gaurav0@aol.com>

--- chargen.js
+++ chargen.js
@@ -4,39 +4,68 @@ function startsWith(str, prefix) {
 
 $(document).ready(function() {
 
-    $("input[type=radio]").each(function(index) {
+    // redraw when any radio button or checkbox is clicked on
+    $("input[type=radio], input[type=checkbox]").each(function() {
         $(this).click(redraw);
     });
     
+    // When radio button is unchecked, its children should be too. 
+    $("input[type=radio]").each(function() {
+        $(this).change(function() {
+            var name = $(this).attr("name");
+            $("input[name=" + name + "]").each(function() {
+                if (!($(this).prop("checked"))) {
+                    $(this).parent().children("ul").find("input[type=checkbox]").each(function() {
+                        $(this).prop("checked", false);
+                    });
+                }
+            });
+        });
+    });
+    
+    // Do not multiple toggle when clicking on children
     $("#chooser>ul>li>ul>li>ul>li").click(function(event) {
         event.stopPropagation();
     });
     
+    // Toggle display of a list elements children when clicked
+    // Do not do so twice, once on label then on input
+    // Again, do not multiple toggle when clicking on children
     $("#chooser>ul>li>ul>li").click(function(event) {
+        if (!($(event.target).get(0).tagName == "LABEL")) {
+            var $ul = $(this).children("ul");
+            $ul.toggle('slow');
+        }
+        event.stopPropagation();
+    });
+    
+    // Toggle display of a list elements children when clicked
+    // Again, do not multiple toggle when clicking on children
+    $("#chooser>ul>li").click(function(event) {
         var $ul = $(this).children("ul");
         $ul.toggle('slow');
         event.stopPropagation();
     });
     
-    $("#chooser>ul>li").click(function() {
-        var $ul = $(this).children("ul");
-        $ul.toggle('slow');
-    });
-    
+    // When clicking on collapse all link, collapse all uls in #chooser
     $("#collapse").click(function() {
         $("#chooser>ul ul").hide('slow');
     });
     
+    // Save canvas as PNG
     $("#saveAsPNG").click(function() {
         Canvas2Image.saveAsPNG(document.getElementsByTagName('canvas')[0]);
     });        
     
     var canvas = $("canvas").get(0);
     var ctx = canvas.getContext("2d");
+    
+    // Determine if an oversize element used
     var oversize = $("input[type=radio]").filter(function() {
         return $(this).data("oversize");
     }).length > 0;
     
+    // Expand canvas if oversize element used
     if (oversize) {
         canvas.width = 1536;
         canvas.height = 1344 + 768;
@@ -45,12 +74,19 @@ $(document).ready(function() {
         canvas.height = 1344;
     }
 
+    // called each time redrawing
     function redraw() {
+        
+        // start over
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+        // determine if an oversize element is being used
         oversize = $("input[type=radio]:checked").filter(function() {
             return $(this).data("oversize");
-        }).length > 0;        
+        }).length > 0;
+
+        // If an oversize element is being used, expand canvas,
+        // otherwise return it to normal size        
         if (oversize) {
             canvas.width = 1536;
             canvas.height = 1344 + 768;
@@ -60,14 +96,21 @@ $(document).ready(function() {
             canvas.height = 1344;
             oversize = false;
         }
-            
-        $("input[type=radio]:checked").filter(function() {
+        
+        // non oversize elements
+        $("input[type=radio]:checked, input[type=checkbox]:checked").filter(function() {
             return !$(this).data("oversize");
         }).each(function(index) {
+        
+            // Determine if male or female selected
             var isMale = $("#sex-male").prop("checked");
             var isFemale = $("#sex-female").prop("checked");
+            
+            // if data-file specified
             if ($(this).data("file")) {
                 var img = getImage($(this).data("file"));
+                
+                // if data-behind specified, draw behind existing pixels
                 if ($(this).data("behind")) {
                     ctx.globalCompositeOperation = "destination-over";
                     ctx.drawImage(img, 0, 0);
@@ -75,6 +118,8 @@ $(document).ready(function() {
                 } else
                     ctx.drawImage(img, 0, 0);
             }
+            
+            // Deal with shield/chain hat overlap issue
             if ($(this).data("file_hat") && $("#hat_chain").prop("checked")) {
                 var img = getImage($(this).data("file_hat"));
                 ctx.drawImage(img, 0, 0);
@@ -83,6 +128,8 @@ $(document).ready(function() {
                 var img = getImage($(this).data("file_no_hat"));
                 ctx.drawImage(img, 0, 0);
             }
+            
+            // if data-file_male and data-file_female is specified
             if (isMale && $(this).data("file_male")) {
                 var img = getImage($(this).data("file_male"));
                 ctx.drawImage(img, 0, 0);
@@ -91,6 +138,8 @@ $(document).ready(function() {
                 var img = getImage($(this).data("file_female"));
                 ctx.drawImage(img, 0, 0);
             }
+            
+            // Draw shadows for plain or ponytail2 hairstyles appropriate to body color
             var id = $(this).attr("id");
             var hsm_prefix = "hair-plain-";
             var hsf_prefix = "hair-ponytail2-";
@@ -111,6 +160,11 @@ $(document).ready(function() {
             }
         });
         
+        // Oversize weapons: Copy existing canvas poses to new locations
+        // with 192x192 padding rather than 64x64
+        // data-oversize="1" means thrust weapon
+        // data-oversize="2" means slash weapon
+        // use appropriate thrust or slash pose
         if (oversize) {
             $("input[type=radio]:checked").filter(function() {
                 return $(this).data("oversize");
@@ -144,7 +198,9 @@ $(document).ready(function() {
             });
         }
         
-        $("input[type=radio]").each(function(index) {
+        // Clear everything if illegal combination used
+        // Probably should try to prevent this
+        $("input[type=radio], input[type=checkbox]").each(function(index) {
             if ($(this).data("required")) {
                 var requirement = $(this).data("required").replace("=", "-");
                 if ($("#" + requirement).prop("checked"))
@@ -167,12 +223,15 @@ $(document).ready(function() {
         });
     }
     
+    // Cache images
     var images = {};
     
     function getImage(imgRef) {
         if (images[imgRef])
             return images[imgRef];
         else {
+        
+            // Load image if not in cache
             var img = new Image();
             img.src = "Universal-LPC-spritesheet/" + imgRef;
             img.onload = redraw;
@@ -181,5 +240,6 @@ $(document).ready(function() {
         }
     }
     
+    // Draw now - on ready
     redraw();
 });
