fb5c4a61ef5feaecd2c512626893dbc4cb574408
bluecarrot16 <27840+bluecarrot16@users.noreply.github.com>
2021-08-24 01:36:00 -0400

    Generate prettier credits text. Can still download as CSV.

--- sources/chargen.js
+++ sources/chargen.js
@@ -250,7 +250,7 @@ $(document).ready(function() {
     if (fileName !== "") {
       let creditEntry = getCreditFor(fileName);
       if (!creditEntry) {
-        sheetCredits.push(fileName+",!MISSING LICENSE INFORMATION! PLEASE CORRECT MANUALY AND REPORT BACK VIA A GITHUB ISSUE,,,,,,,,NOK");
+        sheetCredits.push(fileName+",!MISSING LICENSE INFORMATION! PLEASE CORRECT MANUALY AND REPORT BACK VIA A GITHUB ISSUE,,,,,,,,BAD");
       } else {
         sheetCredits.push(creditEntry);
       }
@@ -259,7 +259,16 @@ $(document).ready(function() {
   }
 
   function displayCredits() {
-    $("textarea#creditsText").val(sheetCredits.join('\n'));
+    let csv = parseCSV(sheetCredits.join('\n'))
+    let out = csv.slice(1).map(function(row) {
+      let urls = row.slice(4,9)
+        .filter(function (x) { return !!x })
+        .map(function (x) { return "    - " + x})
+      // return ("- " + row[0] + " by " + row[2] + ". License(s): " + row[3] + ". \n    + " + 
+      //   urls.join("\n    + ")+"\n")
+      return [`- ${row[0]}: by ${row[2]}. License(s): ${row[3]}. ${row[1]}`].concat(urls).join("\n")
+    })
+    $("textarea#creditsText").val(out.join("\n\n"));
   }
 
   function previewFile(){
@@ -403,6 +412,42 @@ $(document).ready(function() {
     xmlhttp.open("GET", filePath, false);
     xmlhttp.send();
     return xmlhttp.responseText;
+  }
+
+  function parseCSV(str) {
+      var arr = [];
+      var quote = false;  // 'true' means we're inside a quoted field
+
+      // Iterate over each character, keep track of current row and column (of the returned array)
+      for (var row = 0, col = 0, c = 0; c < str.length; c++) {
+          var cc = str[c], nc = str[c+1];        // Current character, next character
+          arr[row] = arr[row] || [];             // Create a new row if necessary
+          arr[row][col] = arr[row][col] || '';   // Create a new column (start with empty string) if necessary
+
+          // If the current character is a quotation mark, and we're inside a
+          // quoted field, and the next character is also a quotation mark,
+          // add a quotation mark to the current column and skip the next character
+          if (cc == '"' && quote && nc == '"') { arr[row][col] += cc; ++c; continue; }
+
+          // If it's just one quotation mark, begin/end quoted field
+          if (cc == '"') { quote = !quote; continue; }
+
+          // If it's a comma and we're not in a quoted field, move on to the next column
+          if (cc == ',' && !quote) { ++col; continue; }
+
+          // If it's a newline (CRLF) and we're not in a quoted field, skip the next character
+          // and move on to the next row and move to column 0 of that new row
+          if (cc == '\r' && nc == '\n' && !quote) { ++row; col = 0; ++c; continue; }
+
+          // If it's a newline (LF or CR) and we're not in a quoted field,
+          // move on to the next row and move to column 0 of that new row
+          if (cc == '\n' && !quote) { ++row; col = 0; continue; }
+          if (cc == '\r' && !quote) { ++row; col = 0; continue; }
+
+          // Otherwise, append the current character to the current column
+          arr[row][col] += cc;
+      }
+      return arr;
   }
 
   function interpretParams() {
