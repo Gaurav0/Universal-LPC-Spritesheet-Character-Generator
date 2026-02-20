c5181f6ce98a17ab2dcc6da04b2ed8d6329dd42e
JaidynReiman <jaidyn@jaidynreiman.net>
2026-02-20 16:57:40 -0600

    * Don't Close Modal on Selecting Color

--- sources/components/tree/ItemWithRecolors.js
+++ sources/components/tree/ItemWithRecolors.js
@@ -52,7 +52,6 @@ export const ItemWithRecolors = {
                     if (recolor.matchBodyColor || (subSelect === selectionGroup && meta.matchBodyColor)) {
                         applyMatchBodyColor(null, recolor);
                     }
-                    rootViewNode.state.showPaletteModal = null;
                     m.redraw();
                 }
             });
